2d730974dd61137fcdb832aeef69a9ea6fba3a3f
IanLawrence <ian@jozian.com>
2026-07-10 19:41:06 -0300

    Fix formatting: collapse blank lines left by removed warns (stree)
    Satisfies the Syntax Tree lint check (--print-width=100).
    
    Refs unicef/supporthub#260

--- plugin.rb
+++ plugin.rb
@@ -59,12 +59,10 @@ after_initialize do
 
       def get_host(request)
         override_url = SiteSetting.supporthub_header_override_url
-
         return override_url if override_url.present?
         host = request.headers["X-Forwarded-Host"] || request.host
         port = request.headers["X-Forwarded-Port"] || request.port
         protocol = request.headers["X-Forwarded-Proto"] || (request.ssl? ? "https" : "http")
-
 
         host = "#{protocol}://#{host}"
         host << ":#{port}" if port != 80 && port != 443
